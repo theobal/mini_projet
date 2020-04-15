d9cdec6f6ec9ec5ae273ec761812e7e93c04ae37
Balestrini Théophile <theophile.balestrini@gmail.com>
2020-04-15 14:48:19 +0200

    TP4 import, TP5 removed

--- mcuconf.h
+++ mcuconf.h
@@ -143,7 +143,7 @@
 #define STM32_GPT_USE_TIM8                  FALSE
 #define STM32_GPT_USE_TIM9                  FALSE
 #define STM32_GPT_USE_TIM11                 TRUE
-#define STM32_GPT_USE_TIM12                 TRUE
+#define STM32_GPT_USE_TIM12                 FALSE
 #define STM32_GPT_USE_TIM14                 FALSE
 #define STM32_GPT_TIM1_IRQ_PRIORITY         7
 #define STM32_GPT_TIM2_IRQ_PRIORITY         7
